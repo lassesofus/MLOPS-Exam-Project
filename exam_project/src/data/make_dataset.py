bc1da79f7c2a579ab0f4bd690cd811b648921687
LouisThygesen <lt@lanab.dk>
2023-01-06 14:28:19 +0100

    make_dataset.py v2

--- exam_project/src/data/make_dataset.py
+++ exam_project/src/data/make_dataset.py
@@ -3,19 +3,11 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
-
-# TODO: Remove unecessary modules
-import numpy as np
 import pandas as pd
-#from sklearn import metrics
-#import transformers
-#import torch
-#from torch.utils.data import Dataset, DataLoader, RandomSampler, SequentialSampler
-from transformers import BertTokenizer
 
-#@click.command()
-#@click.argument('input_filepath', type=click.Path(exists=True))
-#@click.argument('output_filepath', type=click.Path())
+@click.command()
+@click.argument('input_filepath', type=click.Path(exists=True))
+@click.argument('output_filepath', type=click.Path())
 def main(input_filepath=None, output_filepath=None):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
@@ -33,16 +25,17 @@ def main(input_filepath=None, output_filepath=None):
     df_test = df_test.drop('author', axis=1)
     df_train = df_train.drop('author', axis=1)
 
-    # Split labels intro train and test dataframe 
-    df_train_labels = df_sub[df_sub['id'].isin(df_train['id'])] # Debug empty
+    # Get test_labels and make train labels dataframe 
     df_test_labels = df_sub[df_sub['id'].isin(df_test['id'])]
+
+    df_train_labels = pd.DataFrame()
+    df_train_labels['label'] = df_train['label'].tolist()
+    df_train = df_train.drop("label", axis=1)
 
     # Drop id from all dataframes 
     df_test = df_test.drop('id', axis=1)
     df_train = df_train.drop('id', axis=1)
-
     df_test_labels = df_test_labels.drop('id', axis=1)
-    df_train_labels = df_train_labels.drop('id', axis=1)
 
     # One hot encode labels 
     one_hot_test = pd.get_dummies(df_test_labels['label'])
@@ -61,55 +54,36 @@ def main(input_filepath=None, output_filepath=None):
     df_test_labels = df_test_labels.drop(1,axis = 1)
     df_test_labels = df_test_labels.drop(0,axis = 1)
 
-    #df_train_labels = df_train_labels.drop('1',axis = 1)
-    #df_train_labels = df_train_labels.drop('0',axis = 1)
+    df_train_labels = df_train_labels.drop(1,axis = 1)
+    df_train_labels = df_train_labels.drop(0,axis = 1)
 
-    # 
+    # Merge title and text
+    df_train['comment_text'] = df_train['title'] + " " + df_train['text']
+    df_test['comment_text'] = df_test['title'] + " " + df_test['text']
 
+    df_train = df_train.drop('title', axis=1)
+    df_train = df_train.drop('text', axis=1)
 
-    """ Part 2: Defining key variables """
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    MAX_LEN = 200
+    df_test = df_test.drop('title', axis=1)
+    df_test = df_test.drop('text', axis=1)
 
-    """ Part 3: Change to BERT format """
-    def bert_format():
-        comment_text = str(comment_text[index])
-        comment_text = " ".join(comment_text.split())
+    # Collect text and labels 
+    df_train['list'] = df_train_labels['list']
+    df_test['list'] = df_test_labels['list']
 
-        inputs = self.tokenizer.encode_plus(
-            comment_text,
-            None,
-            add_special_tokens=True,
-            max_length=self.max_len,
-            pad_to_max_length=True,
-            return_token_type_ids=True
-        )
-        ids = inputs['input_ids']
-        mask = inputs['attention_mask']
-        token_type_ids = inputs["token_type_ids"]
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # Save as CSV (TODO: Make processed tensor dataset)
+    df_train.to_csv("./exam_project/data/processed/train.csv",index=False)
+    df_test.to_csv("./exam_project/data/processed/test.csv",index=False)
 
 if __name__ == '__main__':
-    #log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    #logging.basicConfig(level=logging.INFO, format=log_fmt)
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
-    #project_dir = Path(__file__).resolve().parents[2]
+    project_dir = Path(__file__).resolve().parents[2]
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
-    #load_dotenv(find_dotenv())
+    load_dotenv(find_dotenv())
 
     main()
