1c09f93d84b2c6cda2833be9d6f57de6805312ab
EVDIO <evangelos.dio@gmail.com>
2023-01-07 20:51:34 +0100

    Train model v1

--- src/data/dataset_class.py
+++ src/data/dataset_class.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn import metrics
 import torch
-from torch.utils.data import Dataset, DataLoader, RandomSampler, SequentialSampler
-from transformers import BertTokenizer, BertModel, BertConfig
+from torch.utils.data import Dataset
+from transformers import BertTokenizer
 import ast
+
 
 class CustomDataset(Dataset):
     def __init__(self, dataframe, tokenizer, max_len):
@@ -27,32 +27,34 @@ class CustomDataset(Dataset):
             add_special_tokens=True,
             max_length=self.max_len,
             pad_to_max_length=True,
-            return_token_type_ids=True
+            return_token_type_ids=True,
         )
-        ids = inputs['input_ids']
-        mask = inputs['attention_mask']
+        ids = inputs["input_ids"]
+        mask = inputs["attention_mask"]
         token_type_ids = inputs["token_type_ids"]
 
         return {
-            'ids': torch.tensor(ids, dtype=torch.long),
-            'mask': torch.tensor(mask, dtype=torch.long),
-            'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
-            'targets': torch.tensor(self.targets[index], dtype=torch.float)
+            "ids": torch.tensor(ids, dtype=torch.long),
+            "mask": torch.tensor(mask, dtype=torch.long),
+            "token_type_ids": torch.tensor(token_type_ids, dtype=torch.long),
+            "targets": torch.tensor(self.targets[index], dtype=torch.float),
         }
 
-if __name__ == "__main__":
-    # For debugging purpose 
-    df_train = pd.read_csv('./exam_project/data/processed/train.csv')
-    df_train['list'] = df_train['list'].apply(lambda x: list(map(int,x.strip('][').split(', '))))
 
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+def get_dataset(path_file):
+    df = pd.read_csv(path_file)
+    df["list"] = df["list"].apply(lambda x: list(map(int, x.strip("][").split(", "))))
+    df = df.reset_index(drop=True)
+
+    print(f"Dataset: {df.shape}")
+
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
     MAX_LEN = 200
 
-    lol = CustomDataset(df_train, tokenizer, MAX_LEN)
-    lol2 = lol.__getitem__(index=[0,1,2])
-
-    #print('RUN MAIN FOR DEBUGGING')
+    dataset = CustomDataset(df, tokenizer, MAX_LEN)
+    return dataset
 
 
+# dataset = get_dataset("data/processed/train.csv")
 
-
+# print(dataset)
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,63 +1,62 @@
-from transformers import BertTokenizer
-from torch import cuda
 from torch.utils.data import Dataset, DataLoader
-import torch 
-
-from exam_project.models.models import BERTClass
-from exam_project.src.models.train_utils import loss_fn 
+from transformers import BertTokenizer
+import torch
 import hydra
 
-@hydra.main(version_base=None, config_name="config.yaml", config_path=".")
+from src.models.models import BERTClass
+from src.models.train_utils import loss_fn
+from src.data.dataset_class import get_dataset
 
+
+@hydra.main(version_base=None, config_name="config.yaml", config_path=".")
 def train(cfg):
     # # Setting up the device for GPU usage
-    device = 'cuda' if cuda.is_available() else 'cpu'
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
     # Sections of config
 
     # Defining some key variables that will be used later on in the training
-    MAX_LEN = cfg.hyperparameters.batch_size.max_len
-    TRAIN_BATCH_SIZE = cfg.hyperparameters.batch_size.train_batch_size
-    VALID_BATCH_SIZE = cfg.hyperparameters.batch_size.valid_batch_size
-    EPOCHS = cfg.hyperparameters.batch_size.epochs
-    LEARNING_RATE = cfg.hyperparameters.batch_size.learning_rate
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    MAX_LEN = cfg.hyperparameters.max_len
+    TRAIN_BATCH_SIZE = cfg.hyperparameters.train_batch_size
+    VALID_BATCH_SIZE = cfg.hyperparameters.valid_batch_size
+    EPOCHS = cfg.hyperparameters.epochs
+    LEARNING_RATE = cfg.hyperparameters.learning_rate
 
-    train_params = {'batch_size': TRAIN_BATCH_SIZE,
-                'shuffle': True,
-                'num_workers': 0
-                }
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+
+    train_params = {"batch_size": TRAIN_BATCH_SIZE, "shuffle": True, "num_workers": 0}
 
     model = BERTClass()
     model.to(device)
 
-    optimizer = torch.optim.Adam(params =  model.parameters(), lr=LEARNING_RATE)
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=LEARNING_RATE)
 
-    train_set = torch.load("data/processed/train_set.pt")
+    path = "data/processed/train.csv"
+    train_set = get_dataset(path)
     training_loader = DataLoader(train_set, **train_params)
 
     for epoch in range(EPOCHS):
 
         model.train()
-        for _,data in enumerate(training_loader, 0):
-            ids = data['ids'].to(device, dtype = torch.long)
-            mask = data['mask'].to(device, dtype = torch.long)
-            token_type_ids = data['token_type_ids'].to(device, dtype = torch.long)
-            targets = data['targets'].to(device, dtype = torch.float)
+        for _, data in enumerate(training_loader, 0):
+            ids = data["ids"].to(device, dtype=torch.long)
+            mask = data["mask"].to(device, dtype=torch.long)
+            token_type_ids = data["token_type_ids"].to(device, dtype=torch.long)
+            targets = data["targets"].to(device, dtype=torch.float)
 
             outputs = model(ids, mask, token_type_ids)
 
             optimizer.zero_grad()
             loss = loss_fn(outputs, targets)
-            if _%5000==0:
-                print(f'Epoch: {epoch}, Loss:  {loss.item()}')
-            
+            if _ % 5000 == 0:
+                print(f"Epoch: {epoch}, Loss:  {loss.item()}")
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-    torch.save(model.state_dict(), 'trained_model.pt')
+    torch.save(model.state_dict(), "trained_model.pt")
 
 
 if __name__ == "__main__":
-   train()
+    train()
