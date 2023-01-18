6a9b1cd750d88a73ba98f13f433bac25b783cca0
LouisThygesen <lt@lanab.dk>
2023-01-18 16:00:19 +0100

    api and pre-commit

--- api/app/main.py
+++ api/app/main.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI
-from fastapi import UploadFile, File
-from google.cloud import storage
-from http import HTTPStatus
 import os
-import torch 
+from http import HTTPStatus
+
+import torch
+from fastapi import FastAPI, File, UploadFile
+from google.cloud import storage
+
 from src.models.model import BERT
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./application_default_credentials.json"
 
 from src.data.data_utils import load_txt_example
 
@@ -19,35 +22,36 @@ class config:
 
     class model_hps:
         def __init__(self):
-            self.device = 'cpu'
+            self.device = "cpu"
             self.max_len = 20
             self.drop_p = 0.3
             self.embed_dim = 768
             self.out_dim = 2
-            self.bert_version = 'bert-base-uncased'
+            self.bert_version = "bert-base-uncased"
 
     class train_hps:
         def __init__(self) -> None:
-            self.device = 'cpu'
+            self.device = "cpu"
             self.max_len = 20
             self.drop_p = 0.3
             self.embed_dim = 768
             self.out_dim = 2
-            self.bert_version = 'bert-base-uncased'
+            self.bert_version = "bert-base-uncased"
 
     class pred_hps:
         def __init__(self) -> None:
-            self.device = 'cpu'
+            self.device = "cpu"
             self.max_len = 20
             self.drop_p = 0.3
             self.embed_dim = 768
             self.out_dim = 2
-            self.bert_version = 'bert-base-uncased'
-        
+            self.bert_version = "bert-base-uncased"
+
+
 @app.post("/")
 async def read_root(data: UploadFile = File(...)):
     """
-    Takes a txt-file as input and makes prediction with 
+    Takes a txt-file as input and makes prediction with
     model saved in Google cloud bucket
 
     :param data: txt-file
@@ -59,8 +63,8 @@ async def read_root(data: UploadFile = File(...)):
         text_binary = await data.read()
         text_example = text_binary.decode("utf-8")
 
-    if not os.path.exists('./temp_files'):
-        os.mkdir('./temp_files')
+    if not os.path.exists("./temp_files"):
+        os.mkdir("./temp_files")
 
     text_path = "./temp_files/temp.txt"
     weights_path = "./temp_files/weights.pt"
@@ -69,15 +73,15 @@ async def read_root(data: UploadFile = File(...)):
     with open(text_path, "w") as f:
         f.write(text_example)
 
-    # Download weights from Google cloud bucket  
+    # Download weights from Google cloud bucket
     if not os.path.exists(weights_path):
-        storage_client = storage.Client('dtu-mlops')
+        storage_client = storage.Client("dtu-mlops")
         bucket = storage_client.get_bucket("news_model_weights")
-        blob = bucket.blob("17-30-07.pt")
+        blob = bucket.blob("weights.pt")
         blob.download_to_filename(weights_path)
 
     # Make config object
-    cfg =  config()
+    cfg = config()
 
     # Load data and tokenize it
     ids, mask, token_type_ids = load_txt_example(cfg, text_path)
@@ -88,15 +92,13 @@ async def read_root(data: UploadFile = File(...)):
     token_type_ids = token_type_ids.to(device, dtype=torch.long)
 
     # Initialize model from weights
-    model = BERT(drop_p=cfg.model.drop_p,
-                 embed_dim=cfg.model.embed_dim,
-                 out_dim=cfg.model.out_dim)
+    model = BERT(drop_p=cfg.model.drop_p)
 
     # Load weights
-    model.load_state_dict(torch.load(weights_path))
+    model.load_state_dict(torch.load(weights_path, map_location=torch.device("cpu")))
     model.to(cfg.pred.device)
 
-    # Run forward pass 
+    # Run forward pass
     model.eval()
 
     with torch.no_grad():
@@ -105,7 +107,7 @@ async def read_root(data: UploadFile = File(...)):
     # Map prediction to label
     prediction = torch.max(outputs, 1)
     prediction = "unreliable" if prediction.indices[0] == 1 else "reliable"
-    
+
     # Return response
     response = {
         "text_example": text_example,
